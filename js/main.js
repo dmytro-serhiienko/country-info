be0387a1c5cd7db2f8895f5c5f478f9f4590b60a
Dmytro Serhiienko <conneciones@gmail.com>
2025-11-30 01:08:27 +0200

    create

---
+++ js/main.js
@@ -0,0 +1,58 @@
+//* Clock
+function updateClock() {
+  const now = new Date();
+  // Отримуємо години, хвилини, секунди
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  // Додаємо 0 спереду, якщо менше 10
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  // Вставляємо у HTML
+  document.getElementById(
+    "clock"
+  ).textContent = `${hours}:${minutes}:${seconds}`;
+}
+// Оновлюємо кожну секунду
+setInterval(updateClock, 1000);
+// Викликаємо одразу при завантаженні
+updateClock();
+
+// * Country
+const container = document.querySelector("#container");
+const capitalEl = document.querySelector("#capital");
+const areaEl = document.querySelector("#area");
+const inputEl = document.querySelector("#country-input");
+const btnEl = document.querySelector("#search-btn");
+
+btnEl.onclick = function () {
+  fetch("https://restcountries.com/v3.1/name/" + inputEl.value.trim())
+    .then((response) => response.json())
+    .then((data) => {
+      capitalEl.textContent = data[0].capital[0];
+      areaEl.textContent = data[0].area;
+    })
+    .catch(() => {
+      capitalEl.textContent = "Wrong name";
+      areaEl.textContent = "-";
+    });
+};
+
+// клік за межами
+window.addEventListener("click", (event) => {
+  if (!event.target.closest(".body-container")) {
+    inputEl.value = "";
+
+    capitalEl.textContent = "Capital";
+    areaEl.textContent = "Area";
+  }
+});
+
+// Enter для пошуку
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    btnEl.click();
+    inputEl.value = "";
+  }
+});
